af4f0d0702150f3a1cc6a7f005532590e904d9fc
tainaraoliver <tr30405@gmail.com>
2024-01-26 17:49:02 -0300

    Ativ 03.js

---
+++ Ativ 03.js
@@ -0,0 +1,27 @@
+let numeroDia = prompt("Digite um número de 1 a 7 para representar um dia da semana:");
+
+if (numeroDia == 1) {
+    console.log("Domingo");
+}
+else if (numeroDia == 2){
+console.log("Segunda feira");
+}
+else if (numeroDia ==3){
+console.log("Terça feira");
+}
+else if (numeroDia == 4){
+console.log("Quarta feira");
+}
+else if (numeroDia == 5){
+console.log("Quinta feira");
+}
+else if (numeroDia == 6){
+console.log("Sexta feira");
+}
+else if (numeroDia == 7){
+console.log("Sábado");
+}
+else {
+    console.log("Número inválido. Por favor, insira um número de 1 a 7.");
+}
+
