03622c4905ef46c488ca3bbd4cb88c83d87ea151
tainaraoliver <tr30405@gmail.com>
2024-01-26 17:44:49 -0300

    Ativ 01.js

---
+++ Ativ 01 .js
@@ -0,0 +1,11 @@
+let idade = prompt("Digite a sua idade:");
+
+
+if (idade < 18) {
+    console.log("Menor de idade.");
+} else if (idade >= 18 && idade < 65) {
+    console.log("Adulto.");
+} else {
+    console.log("Idoso.");
+    }
+
