49a3cfbb3b81f3b755b8ff523b410ad4ac7d1d65
tainaraoliver <tr30405@gmail.com>
2024-01-26 17:46:41 -0300

    Ativ 02.js

---
+++ Ativ 02.js
@@ -0,0 +1,16 @@
+let primeiroNumero = prompt("Digite o primeiro número:");
+let segundoNumero = prompt("Digite o segundo número:");
+
+ 
+if (primeiroNumero > segundoNumero) {
+     console.log("O primeiro número é maior.");
+   
+   }
+   
+else if (primeiroNumero == segundoNumero) {
+    console.log("Os números são iguais");
+}
+    else{
+    console.log("Segundo número e maior");
+    }
+
